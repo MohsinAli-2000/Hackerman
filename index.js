452cbb2c37bfcae4cc77a6f0be3d7a22ae5bd141
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2023-10-08 10:43:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,39 +10,39 @@ const hacking = async () => {
     process.innerHTML = `Hacking ${target} is in progress...<br>`;
     let hackingAccount = new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            process.innerHTML += `Searching the account ${target} ...<br>`
+            process.innerHTML += `> Searching the account ${target} ...<br>`
             resolve();
         },1000)
         setTimeout(()=>{
-            process.innerHTML += `Trying 500K passwords for  ${target} ...<br>`
+            process.innerHTML += `> Trying 500K passwords for  ${target} ...<br>`
             resolve();
         },2000)
         setTimeout(()=>{
-            process.innerHTML += `Password match not found!<br>`
+            process.innerHTML += `> Password match not found!<br>`
             resolve();
         },4000)
         setTimeout(()=>{
-            process.innerHTML += `Another 500K passwords being tried  for  ${target} ...<br>`
+            process.innerHTML += `> Another 500K passwords being tried  for  ${target} ...<br>`
             resolve();
         },5000)
         setTimeout(()=>{
-            process.innerHTML += `Password Match! <br>`
+            process.innerHTML += `> Password Match! <br>`
             resolve();
         },6000)
         setTimeout(()=>{
-            process.innerHTML += `Accessing target account ${target}...<br>`
+            process.innerHTML += `> Accessing target account ${target}...<br>`
             resolve();
         },7000)
         setTimeout(()=>{
-            process.innerHTML += `Stealing the user information ...<br>`
+            process.innerHTML += `> Stealing the user information ...<br>`
             resolve();
         },8000)
         setTimeout(()=>{
-            process.innerHTML += `Blocking the user access to Account...<br>`
+            process.innerHTML += `> Blocking the user access to Account...<br>`
             resolve();
         },9000)
         setTimeout(()=>{
-            process.innerHTML += `Target: ${target} hacked Successfully! <br>`
+            process.innerHTML += `> Target: ${target} hacked Successfully! <br>`
             resolve();
         },11000)
         
